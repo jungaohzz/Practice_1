6cad5657a4cc678fb239b057c32884c481115b26
‘gaojun <1120426446@qq.com>
2019-10-10 22:34:01 +0800

    Fix

--- AutoTest/test2.py
+++ AutoTest/test2.py
@@ -10,6 +10,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys  # 键盘操作
 from selenium.webdriver.common.action_chains import ActionChains  # 鼠标事件
+import pygame
 
 
 # 这是测试百度登录
@@ -17,15 +18,14 @@ from selenium.webdriver.common.action_chains import ActionChains  # 鼠标事件
 
 class Baidulogin(unittest.TestCase):
     def setUp(self):
-        p = "F:\chromedriver.exe"
-        self.driver = webdriver.Chrome(p)
+
+        self.driver = webdriver.Chrome()
         self.driver.get("https://www.baidu.com/")
-        time.sleep(3)
-        self.driver.get_screenshot_as_file("D:\\1.png")
-        self.driver.refresh()
+        # time.sleep(3)
+        #
+        # self.driver.refresh()
 
         self.driver.maximize_window()
-        time.sleep(2)
 
     # def test_login(self):
     #     # 点击首页登录按钮
@@ -44,11 +44,17 @@ class Baidulogin(unittest.TestCase):
     #     time.sleep(5)
 
     def test_set(self):
-        set_menu = self.driver.find_element_by_link_text("设置")
-        ActionChains(self.driver).move_to_element(set_menu).perform()
-        search_set = self.driver.find_element_by_xpath("//*[@id='wrapper']/div[6]/a[1]")
-        ActionChains(self.driver).click(search_set).perform()
-        time.sleep(10)
+        filepath = r"G:\DJ.mp3"
+        pygame.mixer.init()
+        # 加载音乐
+        pygame.mixer.music.load(filepath)
+        pygame.mixer.music.play(start=0.0)
+        pygame.mixer.music.set_volume(1.0)
+        # 播放时长，没有此设置，音乐不会播放，会一次性加载完
+        time.sleep(300)
+        pygame.mixer.music.stop()
+
+
 
 
 if __name__ == '__main__':
--- WebAuto/baoming.py
+++ WebAuto/baoming.py
@@ -12,6 +12,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from tkinter import messagebox
+import pygame   # 播放器
 
 
 
@@ -49,7 +50,7 @@ class Base(unittest.TestCase):
         ).click()
         time.sleep(2)
         # 点击“同意”
-        time.sleep(13)
+        time.sleep(11)
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@class='yzwb-box']//button[@type='button'][1]"))
         ).click()
@@ -73,136 +74,182 @@ class Base(unittest.TestCase):
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@class='ivu-row']/div[3]/div/div/select"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@class='ivu-row']/div[3]/div/div/select/option[10]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 选择“考试方式”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[3]/form/div[2]/div/select"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[3]/form/div[2]/div/select/option[2]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 选择“专项计划”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[3]/form/div[3]/div/select"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[3]/form/div[3]/div/select/option[2]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 选择“报考类别”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[3]/form/div[4]/div/select"))
         ).click()
-        time.sleep(5)
+        time.sleep(1)
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[3]/form/div[4]/div/select/option[2]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 点击“下一步”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[3]/form/div[5]/div/button[2]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 点击“下一步”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[3]/form/div[3]/div/button[2]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 选择“报考院系所名称”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[2]/div[1]/div/select"))
         ).click()
-        time.sleep(5)
+        time.sleep(1)
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[2]/div[1]/div/select/option[9]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 选择“报考专业”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[2]/div[2]/div/select"))
         ).click()
-        time.sleep(5)
+        time.sleep(1)
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[2]/div[2]/div/select/option[2]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 选择“研究方向”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[2]/div[3]/div/select"))
         ).click()
-        time.sleep(5)
+        time.sleep(1)
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[2]/div[3]/div/select/option[3]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 选择“学习方式”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[2]/div[4]/div/select"))
         ).click()
-        time.sleep(5)
+        time.sleep(1)
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[2]/div[4]/div/select/option[2]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 选择“考试科目”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[2]/div[5]/div/select"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[2]/div[5]/div/select/option[2]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
         # 点击“下一步”
         WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@id='app']/div[1]/form[2]/div[4]/div/button[2]"))
         ).click()
-        time.sleep(2)
+        time.sleep(1)
 
 
 
         # 定时（20秒）刷新，检验是否还有位置
         i = 1
         while i <= 120:         # 1h执行180次，设置5h的量共900次
-            time.sleep(2)
-            WebDriverWait(self.driver, 3600).until(
+
+            WebDriverWait(self.driver, 60).until(
                 EC.element_to_be_clickable((By.XPATH, "//div[@class='ivu-row']/div[1]/div/div/select"))
-            ).click()
-            WebDriverWait(self.driver, 3600).until(
+            )
+            time.sleep(1)
+            self.driver.find_element_by_xpath("//div[@class='ivu-row']/div[1]/div/div/select").click()
+
+            WebDriverWait(self.driver, 60).until(
                 EC.element_to_be_clickable((By.XPATH, "//div[@class='ivu-row']/div[1]/div/div/select/option[24]"))
-            ).click()
+            )
+            time.sleep(1)
+            self.driver.find_element_by_xpath("//div[@class='ivu-row']/div[1]/div/div/select/option[24]").click()
 
-            time.sleep(4)
+            time.sleep(2)
 
-            WebDriverWait(self.driver, 3600).until(
+            WebDriverWait(self.driver, 60).until(
                 EC.element_to_be_clickable((By.XPATH, "//div[@class='ivu-row']/div[3]/div/div/select"))
-            ).click()
+            )
+            time.sleep(2)
+            self.driver.find_element_by_xpath("//div[@class='ivu-row']/div[3]/div/div/select").click()
             zigong_text = self.driver.find_element_by_xpath("//div[@class='ivu-row']/div[3]/div/div/select/option[7]").text
             yibin_text = self.driver.find_element_by_xpath("//div[@class='ivu-row']/div[3]/div/div/select/option[18]").text
             if yibin_text != "5132(宜宾学院（本考点报名人数已满，请选择其他考点）)":
+                print(yibin_text)
+                WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, "//div[@class='ivu-row']/div[3]/div/div/select/option[18]"))
+                )
+                time.sleep(0.5)
+                self.driver.find_element_by_xpath("//div[@class='ivu-row']/div[3]/div/div/select/option[18]").click()
+
+                # 播放音乐
+                filepath = r"G:\DJ.mp3"
+                pygame.mixer.init()
+                # 加载音乐
+                pygame.mixer.music.load(filepath)
+                pygame.mixer.music.play(start=0.0)
+                pygame.mixer.music.set_volume(1.0)
+                # 播放时长，没有此设置，音乐不会播放，会一次性加载完
+
                 messagebox.showinfo("提示", "宜宾学院")
+
+                time.sleep(30)
+                pygame.mixer.music.stop()
+
                 time.sleep(600)
+
             elif zigong_text != "5114(四川轻化工大学(自贡校区) （本考点报名人数已满，请选择其他考点）)":
+                print(zigong_text)
+                WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, "//div[@class='ivu-row']/div[3]/div/div/select/option[18]"))
+                ).click()
+
+                # 播放音乐
+                filepath = r"G:\DJ.mp3"
+                pygame.mixer.init()
+                # 加载音乐
+                pygame.mixer.music.load(filepath)
+                pygame.mixer.music.play(start=0.0)
+                pygame.mixer.music.set_volume(1.0)
+                # 播放时长，没有此设置，音乐不会播放，会一次性加载完
+
                 messagebox.showinfo("提示", "轻化工大学")
-                time.sleep(600)
+
+                time.sleep(20)
+                pygame.mixer.music.stop()
+
+                time.sleep(5)
 
             i += 1
 
